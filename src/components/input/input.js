2de6c94f28ba84cc66bff6f983c0b4cf6121cb68
Isaac Ferreira <zaclummys@gmail.com>
2021-03-02 01:21:04 -0300

    Update className logic

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -2,15 +2,5 @@ import React from 'react';
 import InputStyle from './input.css';
 
 export default function Input ({ error, ...props }) {
-    let className;
-
-    if (error) {
-        className = InputStyle.error;
-    }
-
-    return (
-        <div className={className}>
-            <input className={InputStyle.input} {...props} />
-        </div>
-    );
+    return <input className={InputStyle.input} data-error={error} {...props} />
 }
