7dc238372201cca4e85b2e829eb70fd729a6dedc
Isaac Ferreira <zaclummys@gmail.com>
2021-03-02 16:26:57 -0300

    Swap comparison operands

--- src/register-service-worker.js
+++ src/register-service-worker.js
@@ -1,4 +1,4 @@
-if ('production' === process.env.NODE_ENV) {
+if (process.env.NODE_ENV === 'production') {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
             navigator.serviceWorker.register('service-worker.js');
