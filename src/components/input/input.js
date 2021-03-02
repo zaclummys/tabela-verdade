dd183e238dd3341ac3d76a104d4b6a917d79d553
Isaac Ferreira <zaclummys@gmail.com>
2021-03-02 01:26:31 -0300

    Add trailing semicolon

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -2,5 +2,5 @@ import React from 'react';
 import InputStyle from './input.css';
 
 export default function Input ({ error, ...props }) {
-    return <input className={InputStyle.input} data-error={error} {...props} />
+    return <input className={InputStyle.input} data-error={error} {...props} />;
 }
