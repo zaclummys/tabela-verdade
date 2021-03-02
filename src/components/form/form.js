5beb26d99c13a6030492acb1d936799c7af88623
Isaac Ferreira <zaclummys@gmail.com>
2021-03-02 01:20:46 -0300

    Change operator description location

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import FormStyle from './form.css';
+
 import Input from '../input/input';
 import { clearInput, setInput, setError } from '../../redux/actions';
 
@@ -19,15 +21,25 @@ class Form extends React.Component {
 
     render () {
         return (
-            <Input
-                required
-                name="input"
-                type="text"
-                placeholder="Digite a fórmula. Por exemplo, (A ∧ B) → (C ∨ D)."
-                autoComplete="off"
-                error={this.props.error}
-                defaultValue={this.props.input}
-                onChange={(event) => this.onChange(event)} />
+            <div className={FormStyle.form}>
+                <Input
+                    required
+                    name="input"
+                    type="text"
+                    placeholder="Digite a fórmula. Por exemplo, (A ∧ B) → (C ∨ D)."
+                    autoComplete="off"
+                    error={this.props.error}
+                    defaultValue={this.props.input}
+                    onChange={(event) => this.onChange(event)} />
+
+                <div className={FormStyle.description}>
+                    <span className={FormStyle.operation}>Negação: <b>&#xAC;</b>, <b>&#x7E;</b>, <b>&#x21;</b></span>
+                    <span className={FormStyle.operation}>Conjunção: <b>&#x2227;</b>, <b>&#x26;</b>, <b>&#x2A;</b></span>
+                    <span className={FormStyle.operation}>Disjunção: <b>&#x2228;</b>, <b>&#x7C;</b>, <b>&#x2B;</b></span>
+                    <span className={FormStyle.operation}>Implicação: <b>&#x2192;</b>, <b>&#x21D2;</b>, <b>-&#x3E;</b></span>
+                    <span className={FormStyle.operation}>Equivalência: <b>&#x2192;</b>, <b>&#x21D2;</b>, <b>&#x3C;-&#x3E;</b></span>
+                </div>
+            </div>
         );
     }
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,11 +15,6 @@ function HeaderDescription () {
             interpretações possíveis. Além disso, você poderia acompanhar passo a passo o
             desenvolvimento da expressão lógica através das sub-expressões. Você poderá utilizar
             diversos caracteres para compor a expressão lógica.
-            Negação: <b>&#xAC;</b>, <b>&#x7E;</b>, <b>&#x21;</b>.
-            Conjunção: <b>&#x2227;</b>, <b>&#x26;</b>, <b>&#x2A;</b>.
-            Disjunção: <b>&#x2228;</b>, <b>&#x7C;</b>, <b>&#x2B;</b>.
-            Implicação: <b>&#x2192;</b>, <b>&#x21D2;</b>, <b>-&#x3E;</b>.
-            Equivalência: <b>&#x2192;</b>, <b>&#x21D2;</b>, <b>&#x3C;-&#x3E;</b>.
         </p>
     );
 }
