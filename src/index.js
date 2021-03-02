fe6e5bb1eeb65e37159368cfcdf0a3351eaa224f
Isaac Ferreira <zaclummys@gmail.com>
2021-03-02 01:46:06 -0300

    Remove leading slash

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ ReactDOM.render(
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js');
+        navigator.serviceWorker.register('service-worker.js');
     });
 }
 
