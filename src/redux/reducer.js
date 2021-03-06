44cf3a7d64c370e2036771960c239165a6933ca0
Isaac Ferreira <zaclummys@gmail.com>
2021-03-05 22:51:03 -0300

    Update reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,23 +1,60 @@
-import { CLEAR_INPUT, SET_INPUT, SET_ERROR } from './actions';
+import {
+    SET_INPUT,
+    SET_ERROR,
+    ADD_CHARACTER,
+    SET_CURSOR,
+} from './actions';
 
-export default function reducer (state = {}, action) {
+const INITIAL_INPUT = '';
+const INITIAL_ERROR = false;
+const INITIAL_CURSOR = {
+    start: 0,
+    end: 0,
+};
+
+const INITIAL_STATE = {
+    input: INITIAL_INPUT,
+    error: INITIAL_ERROR,
+    cursor: INITIAL_CURSOR,
+};
+
+function shiftCursor (cursor, character) {
+    return {
+        start: cursor.start + character.length,
+        end: cursor.end + character.length,
+    };
+}
+
+function addCharacter (input, cursor, character) {
+    return input.substr(0, cursor.start) + character + input.substr(cursor.end);
+}
+
+export default function reducer (state = INITIAL_STATE, action) {
     switch (action.type) {
-        case CLEAR_INPUT:
-            return {
-                ...state,
-                input: null,
-            };
-
         case SET_INPUT:
             return {
                 ...state,
+                error: false,
                 input: action.input,
+            };
+
+        case SET_CURSOR:
+            return {
+                ...state,
+                cursor: action.cursor,
             };
 
         case SET_ERROR:
             return {
                 ...state,
                 error: action.error,
+            };
+
+        case ADD_CHARACTER:
+            return {
+                ...state,
+                cursor: shiftCursor(state.cursor, action.character),
+                input: addCharacter(state.input, state.cursor, action.character),
             };
 
         default:
