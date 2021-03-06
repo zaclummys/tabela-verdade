4085947cc023530404a12b8c5d73256059a561cd
Isaac Ferreira <zaclummys@gmail.com>
2021-03-05 22:51:12 -0300

    Update actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,23 +1,32 @@
-export const CLEAR_INPUT = 'CLEAR_INPUT';
 export const SET_INPUT = 'SET_INPUT';
+export const SET_CURSOR = 'SET_CURSOR';
 export const SET_ERROR = 'SET_ERROR';
-
-export function clearInput () {
-    return {
-        type: CLEAR_INPUT,
-    };
-}
+export const ADD_CHARACTER = 'ADD_CHARACTER';
 
 export function setInput (input) {
     return {
-        input,
         type: SET_INPUT,
+        input: input,
+    };
+}
+
+export function setCursor (cursor) {
+    return {
+        type: SET_CURSOR,
+        cursor: cursor,
     };
 }
 
 export function setError (error) {
     return {
-        error,
         type: SET_ERROR,
+        error: error,
+    };
+}
+
+export function addCharacter (character) {
+    return {
+        type: ADD_CHARACTER,
+        character: character,
     };
 }
