cf3ec4b6f53086e80398b676e3c039cf7d3a8fc6
Isaac Ferreira <zaclummys@gmail.com>
2021-07-26 00:01:29 -0300

    Remove head class name

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -6,7 +6,7 @@ export function Table (props) {
 }
 
 export function Head (props) {
-    return <thead className={TableStyle.head} {...props} />;
+    return <thead {...props} />;
 }
 
 export function Header (props) {
