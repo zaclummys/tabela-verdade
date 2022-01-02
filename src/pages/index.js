8c6940898728fb4ee794014d8ec0723aec800565
Isaac Luiz Vieira Ferreira <zaclummys@gmail.com>
2022-01-02 17:21:50 -0300

    Wrap with container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,34 @@
 import React from 'react';
 
+import Head from '../components/head/head';
+
 import Footer from '../components/footer/footer';
 import TruthTableContainer from '../components/truth-table-container/truth-table-container';
 
 export default function Index () {
     return (
-        <section className="container mx-auto mt-8">
-            <h1 className="my-4 text-3xl font-bold text-primary">
-                Gerador de tabela-verdade
-            </h1>
+        <>
+            <Head />
 
-            <p className="my-6">
-                Esta é uma ferramenta que automatiza a criação da tabela-verdade de expressões lógicas.
-                Ao digitar, a tabela atualizará automaticamente e exibirá todas as combinações possíveis.
-                Além disso, você poderá acompanhar progressivamente o desenvolvimento da expressão lógica através das
-                sub-expressões.
-                Você poderá utilizar diversos caracteres para compor a expressão lógica.
-                A precedência dos operadores é a seguinte: Variável → Negação → Conjunção → Disjunção → Implicação e
-                Equivalência.
-            </p>
+            <section className="container mx-auto mt-8 px-8">
+                <h1 className="my-4 text-3xl font-bold text-primary">
+                    Gerador de tabela-verdade
+                </h1>
 
-            <TruthTableContainer />
+                <p className="my-6">
+                    Esta é uma ferramenta que automatiza a criação da tabela-verdade de expressões lógicas.
+                    Ao digitar, a tabela atualizará automaticamente e exibirá todas as combinações possíveis.
+                    Além disso, você poderá acompanhar progressivamente o desenvolvimento da expressão lógica através das
+                    sub-expressões.
+                    Você poderá utilizar diversos caracteres para compor a expressão lógica.
+                    A precedência dos operadores é a seguinte: Variável → Negação → Conjunção → Disjunção → Implicação e
+                    Equivalência.
+                </p>
 
-            <Footer />
-        </section>
+                <TruthTableContainer />
+
+                <Footer />
+            </section>
+        </>
     );
 }
