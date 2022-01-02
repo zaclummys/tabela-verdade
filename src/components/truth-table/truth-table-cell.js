fbe5d4006b365f0664ebeed4a3fa4a8ad2851a45
Isaac Luiz Vieira Ferreira <zaclummys@gmail.com>
2022-01-02 17:03:43 -0300

    Make truth table cell text smaller

--- src/components/truth-table/truth-table-cell.js
+++ src/components/truth-table/truth-table-cell.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function TruthTableCell ({ value }) {
     return (
-        <td className="py-2 px-6 text-center font-medium">
+        <td className="py-2 px-6 text-center font-medium text-sm">
             {value ? (
                 <span>V</span>
             ) : (
