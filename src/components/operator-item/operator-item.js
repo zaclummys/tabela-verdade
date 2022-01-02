796df94f588d22098810494eb8bd5a913e4944aa
Isaac Luiz Vieira Ferreira <zaclummys@gmail.com>
2022-01-02 17:20:53 -0300

    Make operator button smaller and rounded

--- src/components/operator-item/operator-item.js
+++ src/components/operator-item/operator-item.js
@@ -25,10 +25,11 @@ export default class OperatorItem extends React.Component {
                     type="button"
                     className="
                         h-7 w-8
-                        font-bold text-white text-center
+                        font-bold text-white text-center text-sm
                         transition duration-75 ease-out
                         ring-opacity-40 ring-primary hover:ring-2
                         bg-primary active:bg-primary-dark
+                        rounded-sm
                     "
                     onClick={this.onButtonClick}>
                     {children}
