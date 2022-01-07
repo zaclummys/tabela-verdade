48980bd03facb569006a060dc215caa211f9415f
Isaac Luiz Vieira Ferreira <zaclummys@gmail.com>
2022-01-06 22:09:46 -0300

    Remove margin

--- src/components/truth-table/truth-table-placeholder.js
+++ src/components/truth-table/truth-table-placeholder.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function TruthTablePlaceholder () {
     return (
-        <p className="my-4 text-center text-sm text-gray-400">
+        <p className="text-center text-sm text-gray-400">
             Digite uma expressão lógica no campo acima para gerar a tabela-verdade.
         </p>
     );
