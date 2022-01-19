e3a0dafbe58432e401de3c4cf8f561cc31fe5c52
Isaac Luiz Vieira Ferreira <zaclummys@gmail.com>
2022-01-19 16:49:45 -0300

    Disable Google Analytics script at head

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
                     googleAnalyticsTrackingId,
                 ],
                 pluginConfig: {
-                    head: true,
+                    head: false,
                 },
             },
         },
