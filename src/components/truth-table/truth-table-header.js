3efbd08e2f4689c4d5eace8af3b5d362d378fb21
Isaac Luiz Vieira Ferreira <zaclummys@gmail.com>
2022-01-19 17:18:35 -0300

    Use normalized expression

--- src/components/truth-table/truth-table-header.js
+++ src/components/truth-table/truth-table-header.js
@@ -3,7 +3,7 @@ import React from "react";
 export function TruthTableHeader ({ expression }) {
     return (
         <th className="py-2 px-6 border-primary-dark font-bold whitespace-nowrap">
-            {expression.present()}
+            {expression}
         </th>
     );
 }
