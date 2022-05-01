ae07331791aae79951a60a3ed5e42c9cc87b4891
Isaac Luiz Vieira Ferreira <zaclummys@gmail.com>
2022-05-01 17:16:45 -0300

    Add environment variable warnings

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 const GATSBY_GOOGLE_SITE_VERIFICATION_KEY = process.env.GATSBY_GOOGLE_SITE_VERIFICATION_KEY;
 const GATSBY_GOOGLE_ANALYTICS_TRACKING_ID = process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_ID;
 
+if (GATSBY_GOOGLE_SITE_VERIFICATION_KEY == null) {
+    console.warn('Environment variable `GATSBY_GOOGLE_SITE_VERIFICATION_KEY` is null');
+}
+
+if (GATSBY_GOOGLE_ANALYTICS_TRACKING_ID == null) {
+    console.warn('Environment variable `GATSBY_GOOGLE_ANALYTICS_TRACKING_ID` is null');
+}
+
 module.exports = {
     name: 'Tabela-verdade',
     metadata: {
