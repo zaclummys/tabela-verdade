4521842f0dcd244ce7bf074b87c2dee3d4aa523b
Isaac Luiz Vieira Ferreira <zaclummys@gmail.com>
2022-05-06 01:10:31 -0300

    Fix value name

--- src/components/truth-table/truth-table-row.js
+++ src/components/truth-table/truth-table-row.js
@@ -4,8 +4,8 @@ import TruthTableCell from './truth-table-cell';
 export default function TruthTableRow ({ values }) {
     return (
         <tr className="transition even:bg-gray-200 hover:bg-primary-dark hover:text-white">
-            {values.map(values => (
-                <TruthTableCell value={values} />
+            {values.map(value => (
+                <TruthTableCell value={value} />
             ))}
         </tr>
     );
