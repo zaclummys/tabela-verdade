58d0337ecd581f1c935b5b4a90fff39db2d7460b
Isaac Ferreira <zaclummys@gmail.com>
2024-01-13 16:08:00 -0300

    Delete files

--- tests/stubs/expressions.js
+++
@@ -1,9 +0,0 @@
-import {
-    Expression,
-    UnaryExpression,
-    BinaryExpression,
-} from '../../src/library/expressions';
-
-export class BadExpression extends Expression {}
-export class BadUnaryExpression extends UnaryExpression {}
-export class BadBinaryExpression extends BinaryExpression {}
