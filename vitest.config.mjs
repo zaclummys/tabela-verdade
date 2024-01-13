21a2f26043972f39945d0820aadd80497d45665a
Isaac Ferreira <zaclummys@gmail.com>
2024-01-13 16:07:07 -0300

    Add alias to vitest

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'node',
+    },
+    resolve: {
+        alias: {
+            '~': path.resolve(__dirname, './src')
+        },
     },
 });
