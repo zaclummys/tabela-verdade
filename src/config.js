71ada3223ef6b94b97be80197371015276d80ca7
Isaac Ferreira <zaclummys@gmail.com>
2024-01-13 19:02:20 -0300

    Update URL environment name

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 import zod from 'zod';
 
 const environmentVariablesSchema = zod.object({
-    NEXT_PUBLIC_VERCEL_URL: zod.string(),
+    NEXT_PUBLIC_URL: zod.string(),
     NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION: zod.string(),
     NEXT_PUBLIC_MICROSOFT_SITE_VERIFICATION: zod.string(),
 });
 
 const environmentVariables = environmentVariablesSchema.parse({
-    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
     NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION,
     NEXT_PUBLIC_MICROSOFT_SITE_VERIFICATION: process.env.NEXT_PUBLIC_MICROSOFT_SITE_VERIFICATION,
 });
 
-export const baseUrl = environmentVariables.NEXT_PUBLIC_VERCEL_URL;
+export const baseUrl = environmentVariables.NEXT_PUBLIC_URL;
 export const googleSiteVerification = environmentVariables.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION;
 export const microsoftSiteVerification = environmentVariables.NEXT_PUBLIC_MICROSOFT_SITE_VERIFICATION;
 
