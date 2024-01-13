5823744b841c2d859126eb0407b369dc3c205a1f
Isaac Ferreira <zaclummys@gmail.com>
2024-01-13 16:33:11 -0300

    Pass expression instead of parser to generator

--- src/library/index.js
+++ src/library/index.js
@@ -8,7 +8,10 @@ export default function generateTruthTable (string) {
     const source = new Source(string);
     const lexer = new PeekableLexer(new Lexer(source));
     const parser = new Parser(lexer);
-    const generator = new Generator(parser);
+
+    const expression = parser.parse();
+
+    const generator = new Generator(expression);
 
     return generator.generate();
 }
